e7f7981dacd760bde2a48f0911c9e78d5a767347
Wen30Park <72847529+Wen30Park@users.noreply.github.com>
2020-11-28 17:38:32 -0600

    Add files via upload

---
+++ FormularioM/src/app/pages/template/template.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PaisService } from 'src/app/services/pais.service';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent implements OnInit {
+
+  usuario = {
+    nombre: 'Jose Guadalupe',
+    apellido: 'Cortes',
+    correo: 'jose17y10@gmail.com',
+    pais: 'CRI',
+    genero: 'M'
+  }
+
+  paises: any[] = [];
+
+  constructor( private paisService: PaisService) { }
+
+  ngOnInit(): void {
+
+    this.paisService.getPaises()
+      .subscribe( paises =>{
+        this.paises = paises;
+
+        this.paises.unshift({
+          nombre: ' [Seleccione Pais]',
+          codigo: ''
+        })
+
+        //console.log(paises);
+      });
+
+  }
+
+  guardar( forma: NgForm){
+    console.log( forma );
+
+    if ( forma.invalid ){
+      Object.values(forma.controls).forEach( control =>{
+        control.markAsTouched();
+      });
+    }
+
+    console.log( forma.value );
+  }
+
+}
